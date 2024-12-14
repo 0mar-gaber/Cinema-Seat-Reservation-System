a57b9ff19d7e47fdaf8c51ff46df40ee012d01da
FaresxAshraf <farsashraf03@gmail.com>
2024-12-15 00:04:35 +0200

    Create form & add screen and label

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -21,3 +21,29 @@ let saveBookingToFile name time selectedSeats ticketID =
     let details = sprintf "Ticket ID: %s\nName: %s\nTime: %s\nReserved Seats:\n%s\n\n" ticketID name time selectedSeats
     File.AppendAllText(filePath, details)
 
+let createForm () =
+    let form = new Form()
+    form.Text <- "Cinema Seat Reservation"
+    form.ClientSize <- Size(1368, 725)
+    form.BackColor <- SystemColors.ActiveCaptionText
+
+    let pictureBox1 = new PictureBox()
+    pictureBox1.Image <- Image.FromFile("D:\\f#\\ConsoleApp1\\ConsoleApp1\\image\\Vector 1 (1).png")
+    pictureBox1.Location <- Point(81, 17)
+    pictureBox1.Size <- Size(906, 142)
+    pictureBox1.SizeMode <- PictureBoxSizeMode.StretchImage
+    form.Controls.Add(pictureBox1)
+
+    let labelScreen = new Label()
+    labelScreen.Text <- "Screen"
+    labelScreen.AutoSize <- true
+    labelScreen.BackColor <- Color.Transparent
+    labelScreen.Font <- new Font("Segoe UI Semibold", 20.0f)
+    labelScreen.ForeColor <- Color.LightSeaGreen
+    labelScreen.Location <- Point(468, 66)
+    form.Controls.Add(labelScreen)
+    labelScreen.BringToFront()
+    
+    
+    
+    form
