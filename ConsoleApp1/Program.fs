6b363f0696a7da080c87174d046b7bacd2ae51b3
amryasser7203 <144290292+amryasser7203@users.noreply.github.com>
2024-12-15 19:25:37 +0200

    add 2d array and ui componnents

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -44,6 +44,80 @@ let createForm () =
     form.Controls.Add(labelScreen)
     labelScreen.BringToFront()
     
-    
+      let seats = Array2D.init 4 9 (fun _ _ -> new PictureBox())
+
+
+    let tableLayoutPanel1 = new TableLayoutPanel()
+    tableLayoutPanel1.Location <- Point(68, 248)
+    tableLayoutPanel1.Size <- Size(951, 260)
+    tableLayoutPanel1.ColumnCount <- 9
+    tableLayoutPanel1.RowCount <- 4
+    tableLayoutPanel1.BackColor <- Color.Transparent
+    for i in 0 .. 8 do
+        tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 11.1f))
+    for i in 0 .. 3 do
+        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25.0f))
+    form.Controls.Add(tableLayoutPanel1)
+
+    let comboBoxTime = new ComboBox()
+    comboBoxTime.Font <- new Font("Segoe UI", 19.0f)
+    comboBoxTime.Location <- Point(1044, 323)
+    comboBoxTime.Size <- Size(278, 27)
+    comboBoxTime.Items.AddRange([| "12 PM"; "03 PM"; "06 PM"; "09 PM" |])
+    comboBoxTime.SelectedIndex <- 0
+    comboBoxTime.DropDownStyle <- ComboBoxStyle.DropDownList
+    form.Controls.Add(comboBoxTime)
+
+    let updateSeats (time: string) =
+        let bookingDetails = readBookingFromFile time
+        tableLayoutPanel1.Controls.Clear()  // Clear the previous seats
+        for row in 0 .. 3 do
+            for col in 0 .. 8 do
+                let seat = new PictureBox()
+                seat.Image <- Image.FromFile("D:\\f#\\ConsoleApp1\\ConsoleApp1\\image\\Group 916.png")
+                seat.Size <- Size(94, 49)
+                seat.SizeMode <- PictureBoxSizeMode.Zoom
+                seat.Image.Tag <- "available"
+                let seatName = sprintf "Row: %d, Column: %d" (row + 1) (col + 1)
+                if bookingDetails |> Array.exists (fun line -> line.Contains(seatName)) then
+                    seat.Image <- Image.FromFile("D:\\f#\\ConsoleApp1\\ConsoleApp1\\image\\Group 940.png")
+                    seat.Image.Tag <- "reserved"
+                seat.Click.Add(fun _ ->
+                    if seat.Image.Tag.ToString() = "available" then
+                        seat.Image <- Image.FromFile("D:\\f#\\ConsoleApp1\\ConsoleApp1\\image\\Group 906.png")
+                        seat.Image.Tag <- "selected"
+                    else if seat.Image.Tag.ToString() = "selected" then
+                        seat.Image <- Image.FromFile("D:\\f#\\ConsoleApp1\\ConsoleApp1\\image\\Group 916.png")
+                        seat.Image.Tag <- "available"
+                    else
+                        MessageBox.Show("Sorry, this seat is already reserved.", "Seat Information") |> ignore
+                )
+                tableLayoutPanel1.Controls.Add(seat, col, row)  // Add the seat to the table layout
+                seats.[row, col] <- seat
+
+    updateSeats (comboBoxTime.SelectedItem.ToString())  // Initial seat setup
+
+    comboBoxTime.SelectedIndexChanged.Add(fun _ ->
+        updateSeats (comboBoxTime.SelectedItem.ToString())  // Update seats on time change
+    )
+
+    let labelName = new Label()
+    labelName.Text <- "Name:"
+    labelName.AutoSize <- true
+    labelName.BackColor <- Color.Transparent
+    labelName.Font <- new Font("Segoe UI Semibold", 20.0f)
+    labelName.ForeColor <- Color.LightSeaGreen
+    labelName.Location <- Point(1044, 136)
+    form.Controls.Add(labelName)
+    labelName.BringToFront()
+
+    let textBoxName = new TextBox()
+    textBoxName.BackColor <- Color.SeaShell
+    textBoxName.Font <- new Font("Segoe UI", 20.0f)
+    textBoxName.Location <- Point(1044, 182)
+    textBoxName.Size <- Size(278, 52)
+    form.Controls.Add(textBoxName)
+
+
     
     form
